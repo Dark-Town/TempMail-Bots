cbbebc06b758f6076ca446011ccc509e46985e9f
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-04-16 15:48:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXX:XXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "24413166"  # Replace this API ID with your actual API ID
+API_HASH = "7d43dfedf3baa98292adb7b3f69cd6bd"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8104653263:AAGBJug5XeN09C4h-bkjTjdSDwzHNJHB9vc"  # Replace this BOT_TOKEN
